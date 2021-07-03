567df341526c5e72e816d618df2bfc62308ac7da
Quintupple <aviaviavi1@yahoo.com>
2021-07-03 22:25:01 +0100

    Added enemy sounds

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    // Reference this class in the enemy scripts, and call the public methods
+    // public EnemyManager enemyManager;
+    // void EnemyDeath() {
+    //         insert death stuff here
+    //      enemyManager.EnemyDeathSound();
+    // }
+
+    AudioSource source;
+    public AudioClip enemyPain;
+    public AudioClip[] enemyAlert;
+    public AudioClip[] enemyDeath;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void EnemyPainSound()
+    {
+        source.PlayOneShot(enemyPain);
+    }
+
+    public void EnemyAlertSound()
+    {
+        source.clip = enemyAlert[Random.Range(0, enemyAlert.Length)];
+        source.PlayOneShot(source.clip);
+    }
+
+    public void EnemyDeathSound()
+    {
+        source.clip = enemyDeath[Random.Range(0, enemyDeath.Length)];
+        source.PlayOneShot(source.clip);
+    }
+}
